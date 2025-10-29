500de7c17ed05d67d7e485be1b03dda3d056f5f8
Li Productions <172457372+goldstac@users.noreply.github.com>
2025-10-29 08:16:43 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,4 +2,6 @@
 int main() {
     int money = 5000;
     std::cout << "I Have $" << money << ":)";
+       int bigMoney = 1000000;
+    std::cout << "I Have $" << bigMoney << "  I'm basically a billionaire now!\n";
 }
